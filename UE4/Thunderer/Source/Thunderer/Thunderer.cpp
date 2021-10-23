0906efd6bd0810809a63d2fe91ccd689f1c3a3b9
PolygonBear <Semenyaka.maxim@yandex.ru>
2021-10-23 22:25:07 +0300

    Init submit

---
+++ UE4/Thunderer/Source/Thunderer/Thunderer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Thunderer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Thunderer, "Thunderer" );
---
+++ UE4/Thunderer/Source/Thunderer/ThundererGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThundererGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THUNDERER_API AThundererGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
